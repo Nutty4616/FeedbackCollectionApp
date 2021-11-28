39a169487b1ed5caf287d9d6cb7b03839ba36760
Nutty4616 <nkosin361@gmail.com>
2021-11-28 23:50:14 +0200

    webhooks sendgrid integration

--- services/emailTemplates/surveyTemplate.js
+++ services/emailTemplates/surveyTemplate.js
@@ -7,12 +7,12 @@ module.exports = (survey) => {
         <div style='text-align: center;'>
           <h3>I'd like your input!</h3>
           <p>Please answer the following question:</p>
-          <p>${survey.body}</p>
+          <h3>${survey.body}</h3>
           <div>
-            <a href="${keys.redirectDomain}/api/surveys/thanks">Yes</a>
+            <a href="${keys.redirectDomain}/api/surveys/${survey.id}/yes">Yes</a>
           </div>
           <div>
-            <a href="${keys.redirectDomain}/api/surveys/thanks">No</a>
+            <a href="${keys.redirectDomain}/api/surveys/${survey.id}/no">No</a>
           </div>
         </div>
       </body>
